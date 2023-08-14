6b51b25a886dab6ca7dea18664fd9913103d821d
cipix37 <staicu.ciprian37@gmail.com>
2023-08-14 22:47:25 +0300

    My bot version 0 - moves randomly every time

--- Chess-Challenge/src/My Bot/MyBot.cs
+++ Chess-Challenge/src/My Bot/MyBot.cs
@@ -1,10 +1,12 @@
 ﻿using ChessChallenge.API;
+using System;
 
 public class MyBot : IChessBot
 {
-    public Move Think(Board board, Timer timer)
-    {
-        Move[] moves = board.GetLegalMoves();
-        return moves[0];
-    }
+	private readonly Random random = new Random();
+	public Move Think(Board board, Timer timer)
+	{
+		Move[] moves = board.GetLegalMoves();
+		return moves[random.Next(moves.Length)];
+	}
 }

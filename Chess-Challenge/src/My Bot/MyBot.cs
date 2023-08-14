8ece27f0c69364ce801bde59f22412412c7caf1d
cipix37 <staicu.ciprian37@gmail.com>
2023-08-14 22:51:24 +0300

    My bot version 1 - simple basic evaluation, depth 4

--- Chess-Challenge/src/My Bot/MyBot.cs
+++ Chess-Challenge/src/My Bot/MyBot.cs
@@ -3,10 +3,124 @@ using System;
 
 public class MyBot : IChessBot
 {
+	// global variables
+	private Board globalBoard;
 	private readonly Random random = new Random();
+	private int maxDepth = 4, currentDepth = 0;
+
 	public Move Think(Board board, Timer timer)
 	{
-		Move[] moves = board.GetLegalMoves();
-		return moves[random.Next(moves.Length)];
+		globalBoard = board;
+		int alpha = -2000;
+		int beta = 2000;
+		int currentPlayer = board.IsWhiteToMove ? 1 : -1;
+		return DeepThink(timer, alpha, beta, currentPlayer).Item1;
+	}
+
+	private (Move, double) DeepThink(Timer timer, double alfa, double beta, int player)
+	{
+		int bestMoveIndex = -1;
+		double bestMoveValue = -2000 * player;
+
+		// determine moves and ending
+		Move[] moves = globalBoard.GetLegalMoves();
+		double[] moveValues = new double[moves.Length];
+		(Move, double)[] m = new (Move, double)[moves.Length];
+
+		// if a leaf is reached return the static evaluation
+		if (currentDepth == maxDepth || globalBoard.IsInCheckmate() || globalBoard.IsDraw())
+		{
+			return (new Move(), EndingEvaluation());
+		}
+
+		for (int k = 0; k < moves.Length; k++)
+		{
+			globalBoard.MakeMove(moves[k]);
+			currentDepth++;
+			moveValues[k] = DeepThink(timer, alfa, beta, -player).Item2;
+			if (player == 1)
+			{
+				if (moveValues[k] > bestMoveValue || (moveValues[k] == bestMoveValue && random.Next(100) < 25))
+				{
+					bestMoveIndex = k;
+					bestMoveValue = moveValues[k];
+				}
+				alfa = Math.Max(alfa, bestMoveValue);
+			}
+			else
+			{
+				if (moveValues[k] < bestMoveValue || (moveValues[k] == bestMoveValue && random.Next(100) < 25))
+				{
+					bestMoveIndex = k;
+					bestMoveValue = moveValues[k];
+				}
+				beta = Math.Min(beta, bestMoveValue);
+			}
+			globalBoard.UndoMove(moves[k]);
+			currentDepth--;
+			if (alfa > beta) break;
+		}
+		for (int i = 0; i < moves.Length; i++)
+		{
+			m[i] = (moves[i], moveValues[i]);
+		}
+		if (bestMoveIndex != -1)
+		{
+			return (moves[bestMoveIndex], bestMoveValue);
+		}
+		return (moves[0], bestMoveValue);
+	}
+
+	private double EndingEvaluation()
+	{
+		int color = globalBoard.IsWhiteToMove ? 1 : -1;
+
+		if (globalBoard.IsInCheckmate())
+		{
+			return -color * (1000 - currentDepth);
+		}
+		if (globalBoard.IsDraw())
+		{
+			return -color * 500 * Math.Sign(StaticEvaluation());
+		}
+		return StaticEvaluation();
+	}
+
+	private double StaticEvaluation()
+	{
+		double result = 0;
+		for (int row = 0; row < 8; row++)
+		{
+			for (int col = 0; col < 8; col++)
+			{
+				Piece p = globalBoard.GetPiece(new Square(col, row));
+				int pieceColor = p.IsWhite ? 1 : -1;
+				if (p.IsPawn)
+				{
+					result += pieceColor * 1;
+				}
+				if (p.IsKnight)
+				{
+					result += pieceColor * 3.5;
+				}
+				if (p.IsBishop)
+				{
+					result += pieceColor * 3.5;
+				}
+				if (p.IsRook)
+				{
+					result += pieceColor * 5;
+				}
+				if (p.IsQueen)
+				{
+					result += pieceColor * 10;
+				}
+				//if (p.IsKing)
+				//{
+				//	result += ColorEvaluation(p) * 10000;
+				//}
+			}
+		}
+		return result;
 	}
 }
